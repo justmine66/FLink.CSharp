8ea5a61fa1412c7b36fea8eebbfbd52021845c77
justmine <lijun@geekbuy.com.cn>
2019-09-25 16:32:13 +0800

    clean up code

--- samples/DataAnalytics/StreamingAnalytics/WordCount/Program.cs
+++ samples/DataAnalytics/StreamingAnalytics/WordCount/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using FLink.Core.Api.Common.Functions;
-using FLink.Core.Util;
-using FLink.Streaming.Api.Environment;
 
 namespace WordCount
 {
@@ -9,21 +6,9 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            var env = ExecutionEnvironment.GetExecutionEnvironment();
+            SocketWindowWordCount.Run();
 
             Console.Read();
-        }
-    }
-
-    class Splitter : IFlatMapFunction<string, Tuple<string, int>>
-    {
-        public void FlatMap(string sentence, ICollector<Tuple<string, int>> output)
-        {
-            var words = sentence.Split(" ");
-            foreach (var word in words)
-            {
-                output.Collect(new Tuple<string, int>(word, 1));
-            }
         }
     }
 }
