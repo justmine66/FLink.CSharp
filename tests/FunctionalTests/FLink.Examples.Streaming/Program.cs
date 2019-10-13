e6d08c1aa18ebb21a436075baf3e9470d5fe9fcf
justmine <3538307147@qq.com>
2019-10-13 10:44:01 +0800

    第一次添加项目FLink.Examples.Streaming

---
+++ tests/FunctionalTests/FLink.Examples.Streaming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FLink.Examples.Streaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
