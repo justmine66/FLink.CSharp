897f6cf6e486d47654250da5d9f32d341a106c1f
justmine <3538307147@qq.com>
2019-10-16 22:13:26 +0800

    添加Map函数

--- src/FLink.Core/Api/Common/Functions/IMapFunction.cs
+++ src/FLink.Core/Api/Common/Functions/IMapFunction.cs
@@ -1,6 +1,17 @@
 ﻿namespace FLink.Core.Api.Common.Functions
 {
-    public interface IMapFunction : IFunction
+    /// <summary>
+    /// Base interface for Map functions. Map functions take elements and transform them, element wise. A Map function always produces a single result element for each input element. Typical applications are parsing elements, converting data types, or projecting out fields. Operations that produce multiple result elements from a single input element can be implemented using the <see cref="IFlatMapFunction{TInput,TOutput}"/>.
+    /// </summary>
+    /// <typeparam name="TInput">Type of the input elements.</typeparam>
+    /// <typeparam name="TOutput">Type of the returned elements.</typeparam>
+    public interface IMapFunction<in TInput, out TOutput> : IFunction
     {
+        /// <summary>
+        /// Takes an element from the input data set and transforms it into exactly one element.
+        /// </summary>
+        /// <param name="value">The input value.</param>
+        /// <returns>The transformed value</returns>
+        TOutput Map(TInput value);
     }
 }
---
+++ src/FLink.Core/Api/Common/Functions/RichMapFunction.cs
@@ -0,0 +1,7 @@
+﻿namespace FLink.Core.Api.Common.Functions
+{
+    public abstract class RichMapFunction<TInput, TOutput>: AbstractRichFunction, IMapFunction<TInput, TOutput>
+    {
+        public abstract TOutput Map(TInput value);
+    }
+}
