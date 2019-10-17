e86076999ec75ea3d554e430ad42263044f0a100
justmine <lijun@geekbuy.com.cn>
2019-10-17 13:51:05 +0800

    添加窗口函数

---
+++ src/FLink.Streaming/Api/Functions/Windowing/Delta/IDeltaFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace FLink.Streaming.Api.Functions.Windowing.Delta
+{
+    /// <summary>
+    /// This interface allows the implementation of a function which calculates the delta between two data points. Delta functions might be used in delta policies and allow flexible adaptive windowing based on the arriving data points.
+    /// </summary>
+    /// <typeparam name="TData">The type of input data which can be compared using this function.</typeparam>
+    public interface IDeltaFunction<in TData>
+    {
+        /// <summary>
+        /// Calculates the delta between two given data points.
+        /// </summary>
+        /// <param name="oldDataPoint">the old data point.</param>
+        /// <param name="newDataPoint">the new data point.</param>
+        /// <returns>the delta between the two given points.</returns>
+        double GetDelta(TData oldDataPoint, TData newDataPoint);
+    }
+}
