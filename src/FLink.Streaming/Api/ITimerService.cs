23557acec011856feb0f61c7c5d52fed58bc0756
justmine <3538307147@qq.com>
2019-11-17 14:42:42 +0800

    完善定时系统

---
+++ src/FLink.Streaming/Api/ITimerService.cs
@@ -0,0 +1,48 @@
+﻿using FLink.Streaming.Api.DataStreams;
+
+namespace FLink.Streaming.Api
+{
+    /// <summary>
+    /// Interface for working with time and timers.
+    /// </summary>
+    public interface ITimerService
+    {
+        /// <summary>
+        /// Gets the current processing time.
+        /// </summary>
+        long CurrentProcessingTime { get; }
+
+        /// <summary>
+        /// Gets the current event-time watermark.
+        /// </summary>
+        long CurrentWatermark { get; }
+
+        /// <summary>
+        /// Registers a timer to be fired when processing time passes the given time.
+        /// Timers can internally be scoped to keys and/or windows. When you set a timer in a keyed context, such as in an operation on <see cref="KeyedStream{TElement, TKey}"/> then that context will also be active when you receive the timer notification.
+        /// </summary>
+        /// <param name="time"></param>
+        void RegisterProcessingTimeTimer(long time);
+
+        /// <summary>
+        /// Registers a timer to be fired when the event time watermark passes the given time.
+        /// Timers can internally be scoped to keys and/or windows. When you set a timer in a keyed context, such as in an operation on <see cref="KeyedStream{TElement, TKey}"/> then that context will also be active when you receive the timer notification.
+        /// </summary>
+        /// <param name="time"></param>
+        void RegisterEventTimeTimer(long time);
+
+        /// <summary>
+        /// Deletes the processing-time timer with the given trigger time. This method has only an effect if such a timer was previously registered and did not already expire.
+        /// Timers can internally be scoped to keys and/or windows. When you delete a timer, It is removed from the current keyed context.
+        /// </summary>
+        /// <param name="time"></param>
+        void DeleteProcessingTimeTimer(long time);
+
+        /// <summary>
+        /// Deletes the event-time timer with the given trigger time. This method has only an effect if such a timer was previously registered and did not already expire.
+        /// Timers can internally be scoped to keys and/or windows. When you delete a timer,it is removed from the current keyed context.
+        /// </summary>
+        /// <param name="time"></param>
+        void DeleteEventTimeTimer(long time);
+    }
+}
---
+++ src/FLink.Streaming/Api/TimeDomain.cs
@@ -0,0 +1,11 @@
+﻿namespace FLink.Streaming.Api
+{
+    /// <summary>
+    /// Specifies whether a firing timer is based on event time or processing time.
+    /// </summary>
+    public enum TimeDomain
+    {
+        EventTime,
+        ProcessingTime
+    }
+}
