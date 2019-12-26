fe574b343c53acdf94437f442df8c759c1d12f6d
justmine <lijun@geekbuy.com.cn>
2019-12-26 10:46:24 +0800

    支持MurMurHash3

--- src/FLink.Core/Util/MathUtils.cs
+++ src/FLink.Core/Util/MathUtils.cs
@@ -1,4 +1,7 @@
-﻿namespace FLink.Core.Util
+﻿using System.IO;
+using System.Text;
+
+namespace FLink.Core.Util
 {
     /// <summary>
     /// Collection of simple mathematical routines.
@@ -16,6 +19,27 @@
             input = (input ^ (input >> 27)) * 0x94d049bb133111ebL;
             input = input ^ (input >> 31);
             return (int)input;
+        }
+
+        /// <summary>
+        /// This function hashes an integer value.
+        /// It is crucial to use different hash functions to partition data across machines and the internal partitioning of data structures. This hash function is intended for partitioning across machines.
+        /// </summary>
+        /// <param name="uniqueString"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static int MurmurHash(string uniqueString, Encoding encoding)
+        {
+            var input = encoding.GetBytes(uniqueString);
+            using var stream = new MemoryStream(input);
+            return MurMurHash3.Hash(stream);
+        }
+
+        public static int MurmurHash(string uniqueString)
+        {
+            var input = Encoding.UTF8.GetBytes(uniqueString);
+            using var stream = new MemoryStream(input);
+            return MurMurHash3.Hash(stream);
         }
     }
 }
---
+++ src/FLink.Core/Util/MurMurHash3.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+
+namespace FLink.Core.Util
+{
+    public static class MurMurHash3
+    {
+        //Change to suit your needs
+        const uint Seed = 144;
+
+        public static int Hash(Stream stream)
+        {
+            const uint c1 = 0xcc9e2d51;
+            const uint c2 = 0x1b873593;
+
+            var h1 = Seed;
+            uint streamLength = 0;
+
+            using (var reader = new BinaryReader(stream))
+            {
+                var chunk = reader.ReadBytes(4);
+                while (chunk.Length > 0)
+                {
+                    streamLength += (uint)chunk.Length;
+                    uint k1;
+                    switch (chunk.Length)
+                    {
+                        case 4:
+                            /* Get four bytes from the input into an uint */
+                            k1 = (uint)
+                                (chunk[0]
+                                 | chunk[1] << 8
+                                 | chunk[2] << 16
+                                 | chunk[3] << 24);
+
+                            /* bitmagic hash */
+                            k1 *= c1;
+                            k1 = Rotl32(k1, 15);
+                            k1 *= c2;
+
+                            h1 ^= k1;
+                            h1 = Rotl32(h1, 13);
+                            h1 = h1 * 5 + 0xe6546b64;
+                            break;
+                        case 3:
+                            k1 = (uint)
+                                (chunk[0]
+                                 | chunk[1] << 8
+                                 | chunk[2] << 16);
+                            k1 *= c1;
+                            k1 = Rotl32(k1, 15);
+                            k1 *= c2;
+                            h1 ^= k1;
+                            break;
+                        case 2:
+                            k1 = (uint)
+                                (chunk[0]
+                                 | chunk[1] << 8);
+                            k1 *= c1;
+                            k1 = Rotl32(k1, 15);
+                            k1 *= c2;
+                            h1 ^= k1;
+                            break;
+                        case 1:
+                            k1 = (uint)(chunk[0]);
+                            k1 *= c1;
+                            k1 = Rotl32(k1, 15);
+                            k1 *= c2;
+                            h1 ^= k1;
+                            break;
+
+                    }
+                    chunk = reader.ReadBytes(4);
+                }
+            }
+
+            // finalization, magic chants to wrap it all up
+            h1 ^= streamLength;
+            h1 = FMix(h1);
+
+            unchecked //ignore overflow
+            {
+                return (int)h1;
+            }
+        }
+
+        private static uint Rotl32(uint x, byte r) => (x << r) | (x >> (32 - r));
+
+        private static uint FMix(uint h)
+        {
+            h ^= h >> 16;
+            h *= 0x85ebca6b;
+            h ^= h >> 13;
+            h *= 0xc2b2ae35;
+            h ^= h >> 16;
+            return h;
+        }
+    }
+}
